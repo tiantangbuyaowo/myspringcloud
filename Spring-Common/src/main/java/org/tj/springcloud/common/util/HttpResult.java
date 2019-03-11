53300f475ee20d4668cb1d16f0636fa0830d3c91
tiantangbuyaowo <954218947@qq.com>
2019-03-11 21:33:36 +0800

    增加工具类

---
+++ Spring-Common/src/main/java/org/tj/springcloud/common/util/HttpResult.java
@@ -0,0 +1,15 @@
+package org.tj.springcloud.common.util;
+
+/**
+ * @author tangjing
+ * @desc
+ * @date 2019/3/11.
+ */
+public class HttpResult {
+
+    public int code;
+
+    public String msg;
+
+    public Object data;
+}
