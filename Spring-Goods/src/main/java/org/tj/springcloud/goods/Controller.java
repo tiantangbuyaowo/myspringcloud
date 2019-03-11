262aea45e0dc0ddf7c5184ef39428969f990c22d
tiantangbuyaowo <954218947@qq.com>
2019-03-11 21:37:40 +0800

    增加工具类

---
+++ Spring-Goods/src/main/java/org/tj/springcloud/goods/Controller.java
@@ -0,0 +1,12 @@
+package org.tj.springcloud.goods;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author tangjing
+ * @desc
+ * @date 2019/3/11.
+ */
+@RestController
+public class Controller {
+}
