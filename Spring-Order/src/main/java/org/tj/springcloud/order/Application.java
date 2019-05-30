191769b310826e1591f6019eb5311fd5fbc7ee51
tangjing <954218947@qq.com>
2019-05-30 14:40:23 +0800

    调整名字

--- Spring-Order/src/main/java/org/tj/springcloud/order/Application.java
+++ Spring-Order/src/main/java/org/tj/springcloud/order/Application.java
@@ -3,14 +3,12 @@ package org.tj.springcloud.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 /**
  * Created by tangjing on 2019/2/27.
  */
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
 public class Application {
 
     public static void main(String[] args) {
