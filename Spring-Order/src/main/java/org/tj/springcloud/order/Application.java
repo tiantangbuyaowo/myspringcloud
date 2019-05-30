565fe7409088ad8351cf035972440ffdb6f621da
tangjing <954218947@qq.com>
2019-05-30 14:38:47 +0800

    调整名字

--- Spring-Order/src/main/java/org/tj/springcloud/order/Application.java
+++ Spring-Order/src/main/java/org/tj/springcloud/order/Application.java
@@ -1,12 +1,9 @@
-package org.tj.springcloud.web;
+package org.tj.springcloud.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 /**
  * Created by tangjing on 2019/2/27.
