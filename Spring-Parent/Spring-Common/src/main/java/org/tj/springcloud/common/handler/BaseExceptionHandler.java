ab87e8f9981e69205fdc2b6de013f86352d7f26d
tangjing <954218947@qq.com>
2020-02-08 22:40:53 +0800

    oauth2权限操作完事

---
+++ Spring-Parent/Spring-Common/src/main/java/org/tj/springcloud/common/handler/BaseExceptionHandler.java
@@ -0,0 +1,21 @@
+package org.tj.springcloud.common.handler;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.tj.springcloud.common.exception.CloudException;
+import org.tj.springcloud.common.util.HttpResult;
+
+@ControllerAdvice
+public class BaseExceptionHandler {
+
+
+    @ExceptionHandler(value = Exception.class)
+    @ResponseBody
+    HttpResult<String> error(Exception e) {
+
+        return HttpResult.ERROR(e.getMessage());
+
+    }
+
+}
---
+++ Spring-Parent/Spring-Common/src/main/java/org/tj/springcloud/common/model/userservice/vo/AddUserFormVo.java
@@ -0,0 +1,8 @@
+package org.tj.springcloud.common.model.userservice.vo;
+
+
+/**
+ * 添加用户所需的Vo对象
+ */
+public class AddUserFormVo {
+}
