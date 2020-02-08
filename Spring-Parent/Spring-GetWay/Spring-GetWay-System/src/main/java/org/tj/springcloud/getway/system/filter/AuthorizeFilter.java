d2bafdc7a58d44b274147931244d4bc74348b16e
tangjing <954218947@qq.com>
2020-02-08 22:26:29 +0800

    oauth2权限操作完事

--- Spring-Parent/Spring-GetWay/Spring-GetWay-System/src/main/java/org/tj/springcloud/getway/system/filter/AuthorizeFilter.java
+++ Spring-Parent/Spring-GetWay/Spring-GetWay-System/src/main/java/org/tj/springcloud/getway/system/filter/AuthorizeFilter.java
@@ -14,50 +14,51 @@ import org.springframework.web.server.ServerWebExchange;
 import org.tj.springcloud.getway.system.util.JwtUtil;
 import reactor.core.publisher.Mono;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 @Component
 public class AuthorizeFilter implements GlobalFilter, Ordered {
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
 
-       if(1==1){
-           return chain.filter(exchange);
-       }
         //1.获取请求对象
         ServerHttpRequest request = exchange.getRequest();
         //2.获取响应对象
         ServerHttpResponse response = exchange.getResponse();
 
         //3.判断当前的请求是否为登录请求,如果是,则直接放行
-        if (request.getURI().getPath().contains("/admin/login")){
+        if (request.getURI().getPath().contains("/oauth/login")) {
             //放行
             return chain.filter(exchange);
         }
         //4.获取当前的所有请求头信息
         HttpHeaders headers = request.getHeaders();
 
-        //5.获取jwt令牌信息
-        String jwtToken = headers.getFirst("token");
+        //客户头文件里不能自己设置直接请求的token，如果自己直接设置的，就直接认为他没权限
+        if (StringUtils.isEmpty(headers.getFirst("Authorization"))) {
+            //5.获取jwt令牌信息
+            String jwtToken = headers.getFirst("token");
 
-        //6.判断当前令牌是否存在,
-        if (StringUtils.isEmpty(jwtToken)){
-            //如果不存在,则向客户端返回错误提示信息
+            //6.判断当前令牌是否存在,
+            if (StringUtils.isEmpty(jwtToken)) {
+                //如果不存在,则向客户端返回错误提示信息
+                response.setStatusCode(HttpStatus.UNAUTHORIZED);
+                return response.setComplete();
+            }
+
+            if (!jwtToken.startsWith("bearer ")) { //如果不是以bearer 开头
+                jwtToken = "bearer " + jwtToken;
+            }
+            // header中添加token数据
+            ServerHttpRequest host = exchange.getRequest().mutate().header("Authorization", jwtToken).build();
+            exchange = exchange.mutate().request(host).build();
+        } else {
             response.setStatusCode(HttpStatus.UNAUTHORIZED);
             return response.setComplete();
         }
 
-        //6.1 如果令牌存在,解析jwt令牌,判断该令牌是否合法,如果令牌不合法,则向客户端返回错误提示信息
-        try {
-            //解析令牌
-            JwtUtil.parseJWT(jwtToken);
-        }catch (Exception e){
-            e.printStackTrace();
-            //令牌解析失败
-            //向客户端返回错误提示信息
-            response.setStatusCode(HttpStatus.UNAUTHORIZED);
-            return response.setComplete();
-        }
-
-        //6.2 如果令牌合法,则放行
+        //6.2 如果令牌存在,则放行
         return chain.filter(exchange);
     }
 
