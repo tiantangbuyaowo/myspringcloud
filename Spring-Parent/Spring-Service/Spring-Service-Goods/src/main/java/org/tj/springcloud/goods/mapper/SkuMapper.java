1c7b0c8e1d2715e2097d1311318513e4d7c0b6da
tangjing <954218947@qq.com>
2020-08-24 10:10:39 +0800

    增加类

---
+++ Spring-Parent/Spring-Service/Spring-Service-Goods/src/main/java/org/tj/springcloud/goods/mapper/SkuMapper.java
@@ -0,0 +1,14 @@
+package org.tj.springcloud.goods.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.tj.springcloud.common.model.goodservice.TbSku;
+
+/**
+ * @author tangjing
+ * @desc
+ * @date 2019/3/11.
+ */
+public interface SkuMapper extends BaseMapper<TbSku> {
+
+
+}
