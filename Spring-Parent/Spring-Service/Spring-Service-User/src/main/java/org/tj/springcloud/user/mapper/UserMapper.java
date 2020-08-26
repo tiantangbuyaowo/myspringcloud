fcbcc65c79429266e01c8996c2ecfd6bcca224ec
tangjing <954218947@qq.com>
2020-08-26 15:13:09 +0800

    使用mybatisplus

--- Spring-Parent/Spring-Service/Spring-Service-User/src/main/java/org/tj/springcloud/user/mapper/UserMapper.java
+++ Spring-Parent/Spring-Service/Spring-Service-User/src/main/java/org/tj/springcloud/user/mapper/UserMapper.java
@@ -1,10 +1,11 @@
 package org.tj.springcloud.user.mapper;
 
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.tj.springcloud.common.model.userservice.User;
 
 /**
  * 用户操作mapper
  */
-public interface UserMapper {
+public interface UserMapper extends BaseMapper<User> {
     User findUserByLogInName(String logInName);
 }
