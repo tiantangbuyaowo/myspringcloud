5e3a4ddbd263045920ba315c658cf18dd14e90c6
tangjing <954218947@qq.com>
2020-08-26 16:10:39 +0800

    增加一个日志打印

--- Spring-Parent/Spring-User-Oauth/src/main/java/org/tj/springcloud/user/oauth/client/back/UserServiceFallback.java
+++ Spring-Parent/Spring-User-Oauth/src/main/java/org/tj/springcloud/user/oauth/client/back/UserServiceFallback.java
@@ -1,5 +1,6 @@
 package org.tj.springcloud.user.oauth.client.back;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.tj.springcloud.common.util.HttpResult;
 import org.tj.springcloud.user.oauth.client.UserClient;
@@ -7,11 +8,14 @@ import org.tj.springcloud.user.oauth.client.UserClient;
 /**
  * Created by tangjing on 2019/6/4.
  */
+@Slf4j
 @Component
 public class UserServiceFallback implements UserClient {
 
     @Override
     public HttpResult findUserByLogInName(String logInName) {
-        return HttpResult.ERROR("服务器忙");
+
+        log.error("load user-service  findUserByLogInName error");
+        return HttpResult.ERROR("服务器忙,请稍后再试");
     }
 }
